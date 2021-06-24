e9f751472dd0f0ec250f51049f72aad2aa384aeb
No-more-pain <abdoubouz8008@gmail.com>
2021-06-24 23:57:58 +0200

    first commit

---
+++ lib/pages/FoodPge.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+
+
+
+
+class Food_page extends StatefulWidget {
+  const Food_page({Key? key}) : super(key: key);
+
+  @override
+  _Food_pageState createState() => _Food_pageState();
+}
+
+class _Food_pageState extends State<Food_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
